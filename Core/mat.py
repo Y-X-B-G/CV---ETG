38b15fd6d6b7382937b1843b7d58160f2334a43e
Bill Guo <youxguo@gmail.com>
2025-01-05 16:09:56 -0500

    testing loading images

--- Core/mat.py
+++ Core/mat.py
@@ -1,2 +1,10 @@
-""" Opencv library import"""
-import cv2
+""" Opencv and numpy library import"""
+import cv2 as cv
+import numpy as np
+
+#adding path to folder containing data
+cv.samples.addSamplesDataSearchPath("C:/Users/Bill/Documents/Personal (sept 3, 2023)/Programming/Learning opencv/Core/Enemies")
+#importing data theres an bug with pylint but this works
+bullet = cv.imread(cv.samples.findFile("BulletKin.png"), flags=cv.IMREAD_COLOR)
+
+print(bullet[1][1])
